eb1be09b795b07670e97eef3a61c734a5b933ebf
Shreyas Khandare <120813211+shreyas29032001@users.noreply.github.com>
2025-07-04 16:47:47 +0530

    Delete src directory

--- src/main/java/com/example/ElectronixHub/Repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ElectronixHub.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.ElectronixHub.Model.Product;
-
-public interface ProductRepository extends JpaRepository<Product,Integer>{
-
-}
